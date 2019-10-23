0a7fc405072d6a4fa0a092bf25c2c1c6038d6dc2
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2019-10-22 22:38:24 -0300

    Update commands

--- SD/docker-example/index.py
+++ SD/docker-example/index.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    response = f'Hello World from Server {os.environ.get("NUM_SERVER")}'
+    return response
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    app.run(host = "0.0.0.0", port = int("5000"), debug = True)
